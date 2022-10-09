4f1fd8c6b7d4774c95b330f2643eceb7dedf5f56
Devon G <devgough@gmail.com>
2022-10-08 21:32:10 -0400

    Added .env support

--- temp.py
+++ temp.py
@@ -1,2 +1,14 @@
 # TODO Get eth blockchain working on server
 # TODO Communicate with said chain
+
+import os
+from dotenv import load_dotenv
+from web3 import Web3
+
+load_dotenv()
+
+MAINNET_ENDPOINT=os.getenv('MAINNET_ENDPOINT')
+
+w3 = Web3(Web3.HTTPProvider('http://192.168.0.12:8545'))
+
+print(MAINNET_ENDPOINT)
