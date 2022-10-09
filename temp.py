2d5a44e0b1e6a6fea58561ed7437e207d391f188
Devon G <devgough@gmail.com>
2022-10-08 22:18:11 -0400

    Added important data

--- temp.py
+++ temp.py
@@ -1,6 +1,5 @@
 # TODO Get eth blockchain working on server
 # TODO Communicate with said chain
-print("devon likes big dicks")
 
 import os
 from dotenv import load_dotenv
@@ -12,4 +11,31 @@ MAINNET_ENDPOINT=os.getenv('MAINNET_ENDPOINT')
 
 w3 = Web3(Web3.HTTPProvider(MAINNET_ENDPOINT))
 
-print(w3.isConnected())
+# Gets latest block.
+""" 
+Block Structure:
+
+AttributeDict({
+    'baseFeePerGas' : int,
+    'difficulty' : int,
+    'extraData' : HexBytes,
+    'gasLimit' : int,
+    'gasUsed' : int,
+    'hash' : HexBytes,
+    'logsBloom' : HexBytes,
+    'miner' : HexBytes,
+    'mixHash' : HexBytes,
+    'nonce' : int,
+    'parentHash' : HexBytes,
+    'receiptsRoot' : HexBytes,
+    'sha3Uncles' : HexBytes,
+    'size' : int,
+    'stateRoot' : HexBytes,
+    'timestamp' : int,
+    'totalDifficulty' : int,
+    'transactions' : List[HexBytes],
+    'transactionsRoot' : HexBytes,
+    'uncles' : ? (maybe list)
+})
+ """
+print(w3.eth.get_block('latest').values())
