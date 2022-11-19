8f1d1fc40cc86e17cac40acaa11d7a663d152973
Devon G <devgough@gmail.com>
2022-11-19 16:55:50 -0500

    Added requirements.txt

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -30,7 +30,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/devy/Documents/Code/SohpisticadoTools/.venv"
+set -gx VIRTUAL_ENV "/home/devy/Documents/Code/SophisticadoTools/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
