87e5ef9bad84e882fc7ed699b049bf4d60d5038b
Thomas Kulin <thomas.kulin@yahoo.ca>
2022-11-28 17:57:12 -0500

    account for tx fees in p/l

--- csvPL.py
+++ csvPL.py
@@ -6,25 +6,28 @@ import sys, getopt
 
 class Token2:
 
-    def __init__(self, symbol, pairCurrency, dateBought):
+    def __init__(self, symbol, pairCurrency, dateSold):
 
         self.symbol = symbol
         self.pair = pairCurrency
-        self.dateBought = dateBought
+        self.dateSold = dateSold
 
         self.balanceHistory = []
         self.balance = 0
         self.p_l = 0  # buying is loss, selling is profit
+        self.fees = 0  # track tx fees separately from p/l because there is no guarantee that p/l pair is fee currency
 
-    def buy(self, amount, value=0):
+    def buy(self, amount, value=[0, 0]):
         self.balance += amount
         self.balanceHistory.append(self.balance)
-        self.p_l -= value  # value of the tx in pair currency
+        self.p_l -= value[0]  # value of the tx in pair currency
+        self.fees += value[1]
 
-    def sell(self, amount, value=0):
+    def sell(self, amount, value=[0, 0]):
         self.balance -= amount
         self.balanceHistory.append(self.balance)
-        self.p_l+= value
+        self.p_l+= value[0]
+        self.fees += value[1]
 
 
 class Portfolio2:
@@ -54,23 +57,28 @@ class Portfolio2:
                 return i
         return self.addToken(symbol, pCurr=pCurr, date=date)
 
-    def buy(self, symbol, amount, value=0, pCurr='ETH', txFee=0, feeCurrency='ETH', date=None):
-        token = self.findTokenBySymbol(symbol, pCurr, date)
-        token.buy(amount, value=0 if symbol in self.pairs else value) # dont track p_l on base currencies
-        self.findTokenBySymbol(feeCurrency, pCurr, date).sell(txFee)
+    def buy(self, symbol, amount, value, pCurr='ETH', txFee=0, feeCurrency='ETH', date=None):
+        token = self.findTokenBySymbol(str(symbol), pCurr, date)
+        if token.symbol == 'PSYOP':
+            print('breakpoint')
+        # I should subtract TX fees from value for inclusion in P/L calculation but there is no way to reconcile different pairs. TOKEN could be paired with USDC and tx fee is in eth :////
+        token.buy(amount, value=[0, 0] if symbol in self.pairs else [value, txFee])  # dont track p_l on base currencies
+        self.findTokenBySymbol(str(feeCurrency), pCurr, date).sell(txFee)
         # print("New", token.symbol, "balance: ", str(token.balance), token.symbol)
 
-    def sell(self, symbol, amount, value=0, pCurr='ETH', txFee=0, feeCurrency='ETH', date=None):
-        token = self.findTokenBySymbol(symbol, pCurr, date)
-        token.sell(amount, value=0 if symbol in self.pairs else value) # dont track p_l on base currencies
-        self.findTokenBySymbol(feeCurrency, pCurr, date).sell(txFee)
+    def sell(self, symbol, amount, value, pCurr='ETH', txFee=0, feeCurrency='ETH', date=None):
+        token = self.findTokenBySymbol(str(symbol), pCurr, date)
+        if token.symbol == 'PSYOP':
+            print('breakpoint')
+        token.sell(amount, value=[0, 0] if symbol in self.pairs else [value, txFee])  # dont track p_l on base currencies
+        self.findTokenBySymbol(str(feeCurrency), pCurr, date).sell(txFee)
         # print("New", token.symbol, "balance: ", str(token.balance), token.symbol)
 
 ### handle input args ###
 argv = sys.argv[1:]
 outfile = 'output.xlsx'
-infile = 'brokeninput.csv'
-mode = 'token'
+infile = '3ab3.csv'
+mode = 'day'
 try:
       opts, args = getopt.getopt(argv,"hm:i:o:",["ifile=","ofile="])
 except getopt.GetoptError:
@@ -128,60 +136,68 @@ for i, r in parsed.iterrows():
         sCurr = str(sCurr).split('\r\n')
 
         for i in range(len(bCurr)):
-            p.buy(bCurr[i], float(bAmount[i]), value=float(sAmount[i] if i < len(sAmount) else 0), pCurr=sCurr[0], txFee=float(fAmount) if i==0 else 0, feeCurrency=fCurr, date=date)
+            # if bCurr[i] == 'PSYOP':
+            #     print('breakpoint')
+            p.buy(bCurr[i], float(bAmount[i]), float(sAmount[i]) if i < len(sAmount) else 0, pCurr=sCurr[0], txFee=float(fAmount) if i==0 else 0, feeCurrency=fCurr, date=date)
         for i in range(len(sCurr)):
-            p.sell(sCurr[i], float(sAmount[i]), value=float(bAmount[i] if i < len(bAmount) else 0), pCurr=bCurr[0], date=date)
+            p.sell(sCurr[i], float(sAmount[i]), float(bAmount[i]) if i < len(bAmount) else 0, pCurr=bCurr[0], date=date)
 
 p_l_pertoken = []
 p_l_perday = []
-cum_pl = [[None], [0]]  # cumulative p/l per pair
-daily_pl = [[None], [0]]  # p/l per day, per pair (caclulated by date of purchase, not sell)
+cum_pl = [[None], [0], [0]]  # cumulative p/l per pair [[pair], [p/l], [fees]]
+daily_pl = [[None], [0], [0]]  # p/l per day, per pair (caclulated by date of purchase, not sell)
 dates = [None]  # list of dates
 
 def addDailyPL(token):
     if t.pair not in daily_pl[0]:  # make sure pair is in list of pairs
         daily_pl[0].append(t.pair)
         daily_pl[1].append(0)
+        daily_pl[2].append(0)
     for i in daily_pl[0]:  # iterate through pairs
         if i == t.pair:
             idx = daily_pl[0].index(i)
             daily_pl[1][idx] += t.p_l
+            daily_pl[2][idx] += t.fees
 
 def printDailyPL():
     daily_pl[0].pop(0)  # remove none pair
     daily_pl[1].pop(0)
+    daily_pl[2].pop(0)
     for i in daily_pl[0]:  # print all p/l pairs for the previous day
         idx = daily_pl[0].index(i)
-        p_l_perday.append([dates[-1] if idx == 0 else '', daily_pl[1][idx], i])
+        p_l_perday.append([dates[-1] if idx == 0 else '', daily_pl[1][idx], i, daily_pl[2][idx], 'ETH'])
 
 for t in p.tokens:
     if t.pair not in cum_pl[0]:  # make sure pair is in list of pairs
         cum_pl[0].append(t.pair)
         cum_pl[1].append(0)
+        cum_pl[2].append(0)
     for i in cum_pl[0]:  # iterate through pairs and accumulate p/l corresponding to that pair
         if i==t.pair:
             idx = cum_pl[0].index(i)
             cum_pl[1][idx] += t.p_l
-            p_l_pertoken.append([t.symbol, t.balance, t.p_l, cum_pl[1][idx], t.pair])
+            cum_pl[2][idx] += t.fees
+            p_l_pertoken.append([t.symbol, t.balance, t.p_l, cum_pl[1][idx], t.pair, t.fees, cum_pl[2][idx], 'ETH'])
 
-    date = t.dateBought
+    date = t.dateSold
     if date in dates:
         addDailyPL(t)
     else:   # new date added. print previous day
         printDailyPL()
-        daily_pl = [[None], [0]]
+        daily_pl = [[None], [0], [0]]
         dates.append(date)
         addDailyPL(t)
 printDailyPL()
 
 
 
-if mode=='token':
-    pertoken = pd.DataFrame(p_l_pertoken, columns=['Symbol', 'Balance', 'P/L', 'Cumulative P/L', 'Pair'])
+if mode == 'token':
+    pertoken = pd.DataFrame(p_l_pertoken, columns=['Symbol', 'Balance', 'P/L', 'Cumulative P/L', 'Token Pair', 'Fees', 'Cumulative Fees', 'Fee Pair'])
     pertoken.to_excel(outfile, encoding='utf-8')  # write to excel
     print(pertoken)
-elif mode=='day':
-    perday = pd.DataFrame(p_l_perday, columns=['Date', 'P/L', 'Pair'])
+
+elif mode == 'day':
+    perday = pd.DataFrame(p_l_perday, columns=['Date', 'P/L', 'Pair', 'Fees', 'Fee Pair'])
     perday.to_excel(outfile, encoding='utf-8')
     print(perday)
 else:
